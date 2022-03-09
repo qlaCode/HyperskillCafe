37545e4ec169ceb1fd380ba8f6ad310a31821aaf
qlaCode <91269485+qlaCode@users.noreply.github.com>
2022-03-09 21:52:22 +0100

    initial Upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//Variables
+const reviews = document.querySelector("#reviewsRow");
+const reviewTitle = document.querySelector("#reviewTitle");
+const reviewText = document.querySelector("#reviewText");
+const reviewName = document.querySelector("#reviewName");
+const reviewForm = document.getElementById("reviewForm");
+
+//Event
+reviewForm.addEventListener("submit", addReview);
+
+//Function
+function addReview(event){
+
+    event.preventDefault()
+
+    const newReview  = `<div class="col-lg-4 col-md-6 py-3">
+            <div class="card h-100">
+                <div class="card-body">
+                    <h4 class="card-title">${reviewTitle.value}</h4>
+                    <blockquote class="blockquote mb-0">
+                        <p class="card-text">${reviewText.value}</p>
+                        <footer class="blockquote-footer text-end"><i>${reviewName.value || "Anonymous"}</i></footer>
+                    </blockquote>
+                </div>
+            </div>
+        </div>`;
+
+    reviews.innerHTML += newReview;
+
+}
